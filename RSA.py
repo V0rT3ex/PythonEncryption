eee6b651a6fb4ee4bdeb1070f3c9dcf7fef805f4
hi <hi@gmail.com>
2019-02-28 08:41:19 +0200

    Will add is_prime function

--- RSA.py
+++ RSA.py
@@ -2,6 +2,25 @@ from math import gcd
 from random import randint
 
 
+def is_prime(n):
+    """
+    This function receives a number - n.
+    It returns whether n is prime or not.
+    Time complexity is O(n**0.5) in the worst case.
+    """
+
+    if n == 2 or n == 3:
+        return True
+    elif n < 2 or n % 2 == 0 or n % 3 == 0:
+        return False
+
+    # Looping until i reaches the square root of n
+    for i in range(4, int(n ** 0.5)):
+        if n % i == 0:
+            return False
+    return True
+
+
 def generate_keys(n, phi):
     """
     This function receives the modulus(n) and the totient(phi)
